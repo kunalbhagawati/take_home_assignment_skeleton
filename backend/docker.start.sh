d10eaec845d01ad769822b53be433a5851732db6
Kunal Bhagawati <kunal.bhagawati@yahoo.com>
2020-05-23 00:58:22 +0530

    Create new project.

---
+++ backend/docker.start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# This is your start script.
+# This should contain the same set of commands that you would use every time you deploy the app to production.
+# You should replace all the commands below with your commands.
+
+# Setup your dependencies.
+
+# Add your `rails s` or `./manage.py runserver` or such command here.
---
+++ client/docker.start.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This is your start script.
+# This should contain the same set of commands that you would use every time you deploy the app to production.
+# You should replace all the commands below with your commands.
+
+# You are in /code inside the container
+
+# set up your dependencies
+
+# Start your app
