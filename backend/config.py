e82efabcbd11554120220d72db889913977631be
Kunal Bhagawati <kunal.bhagawati@yahoo.com>
2020-05-25 11:46:14 +0530

    f

--- backend/config.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy.engine.url import URL
-
-SQLALCHEMY_DATABASE_URI = str(URL('postgres',
-                                  username='take_home',
-                                  password='take_home',
-                                  host='postgres',
-                                  database='take_home',
-                                  port=5432))
--- tests/Readme.md
+++
@@ -1,2 +0,0 @@
-This is only for the functional and acceptability tests for the requirements given to you.
-You are free to add more tests, but you cannot change the tests already given here.
